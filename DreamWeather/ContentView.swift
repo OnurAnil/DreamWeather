a9c8cf082d065cece251d9f5e57eac5ff5508d65
Onur Anıl <onuranil749@gmail.com>
2024-06-09 18:32:40 +0300

    Initial Commit

---
+++ DreamWeather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DreamWeather
+//
+//  Created by Onur Anıl on 9.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DreamWeather/DreamWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamWeatherApp.swift
+//  DreamWeather
+//
+//  Created by Onur Anıl on 9.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct DreamWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
